68eeb80ba551b6a8d4fcb66e0f5e6e4d3516b1e1
Davi Teixeira <daviteixeira.btm@gmail.com>
2020-08-16 21:02:17 -0300

    Aula 04 - Métodos Getter, Setter e Construtor.

---
+++ CursoEmVideo/Aula04/src/main/java/davi/aula04/Aula04.java
@@ -0,0 +1,9 @@
+
+package davi.aula04;
+
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", 0.4f, "amarelo");
+        c1.status();
+    }
+}
---
+++ CursoEmVideo/Aula04/src/main/java/davi/aula04/Caneta.java
@@ -0,0 +1,54 @@
+package davi.aula04;
+
+public class Caneta {
+    public String modelo;
+    private float ponta;
+    private String cor;
+    private boolean tampada;
+
+    public Caneta(String modelo, float ponta, String cor) {
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+    
+    public void status(){
+        System.out.println("SOBRE A CANETA:");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada? " + this.tampada);
+    }
+}
