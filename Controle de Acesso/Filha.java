375309b9ae3bef5ab953771f1edea95964fc1ac9
Davi Teixeira <daviteixeira.btm@gmail.com>
2020-12-10 14:19:14 -0300

    Controle de Acesso

---
+++ Controle de Acesso/Filha.java
@@ -0,0 +1,13 @@
+package davi.controledeacesso;
+
+/* @author Davi Teixeira */
+
+public class Filha extends Mae{
+    protected int exemplo;
+    protected int getExemplo(){
+        return this.exemplo;
+    }
+    protected void setExemplo(int x){
+        this.exemplo = x;
+    }
+}
---
+++ Controle de Acesso/Mae.java
@@ -0,0 +1,13 @@
+package davi.controledeacesso;
+
+/* @author Davi Teixeira */
+
+public class Mae{
+    public int exemplo;
+    public int getExemplo(){
+        return exemplo;
+    }
+    public void setExemplo(int x){
+        this.exemplo = x;
+    } 
+}
